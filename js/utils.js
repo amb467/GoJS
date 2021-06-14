b0d5660a8ddd069fb8752bc94a209d57971b7dcd
Amanda Work <amandawork@MacBook-Air.local>
2021-06-14 15:12:57 -0400

    First commit

---
+++ js/utils.js
@@ -0,0 +1,32 @@
+    // Define a function for creating a "port" that is normally transparent.
+    // The "name" is used as the GraphObject.portId,
+    // the "align" is used to determine where to position the port relative to the body of the node,
+    // the "spot" is used to control how links connect with the port and whether the port
+    // stretches along the side of the node,
+    // and the boolean "output" and "input" arguments control whether the user can draw links from or to the port.
+    function makePort($, name, align, spot, output, input) {
+      var horizontal = align.equals(go.Spot.Top) || align.equals(go.Spot.Bottom);
+      // the port is basically just a transparent rectangle that stretches along the side of the node,
+      // and becomes colored when the mouse passes over it
+      return $(go.Shape,
+        {
+          fill: "transparent",  // changed to a color in the mouseEnter event handler
+          strokeWidth: 0,  // no stroke
+          width: horizontal ? NaN : 8,  // if not stretching horizontally, just 8 wide
+          height: !horizontal ? NaN : 8,  // if not stretching vertically, just 8 tall
+          alignment: align,  // align the port on the main Shape
+          stretch: (horizontal ? go.GraphObject.Horizontal : go.GraphObject.Vertical),
+          portId: name,  // declare this object to be a "port"
+          fromSpot: spot,  // declare where links may connect at this port
+          fromLinkable: output,  // declare whether the user may draw links from here
+          toSpot: spot,  // declare where links may connect at this port
+          toLinkable: input,  // declare whether the user may draw links to here
+          cursor: "pointer",  // show a different cursor to indicate potential link point
+          mouseEnter: function(e, port) {  // the PORT argument will be this Shape
+            if (!e.diagram.isReadOnly) port.fill = "rgba(255,0,255,0.5)";
+          },
+          mouseLeave: function(e, port) {
+            port.fill = "transparent";
+          }
+        });
+    }
